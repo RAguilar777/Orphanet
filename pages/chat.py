6dfebbf49d40fc2edb2b30e90000673568037ebc
Robert Aguilar <62629630+RAguilar777@users.noreply.github.com>
2022-09-12 14:44:45 -0400

    Final

--- pages/chat.py
+++ pages/chat.py
@@ -1,10 +1,49 @@
+from multiprocessing.resource_sharer import stop
 import streamlit as st
-from streamlit_discourse import st_discourse
 
+# A very basic version of a comment section for Orphanet
+st.set_page_config(page_title="Chat", page_icon="😎", layout="wide")
 st.title("Chat With Others")
+st.header("Get to know others like you")
+
+st.write("##")
+forum = st.container()
+commentBox= st.container()
+
+# Forum Area
+with forum:
+    forum.subheader("Forum")
+    forum.write("Anna")
+    forum.write("Hello my name is Anna, nice to meet you George!")
+
+    forum.write("#")
+
+    forum.write("George")
+    forum.write("Hi Anna, nice to meet you too.")
+
+st.write("##")
+
+# Generates the user's comment
+def createComment(name, data):
+    if (len(data) <= 250):
+        forum.write("#")
+        forum.write(name)
+        forum.write(data)
+        st.success("Your comment was successfully posted.")
+
+    else:
+        st.warning("Comments should be no more than 250 characters.")
+        return
 
 
-discourse_url = "discuss.streamlit.io"
-topic_id = 1726
+# Comment submission form
+with commentBox:
+    st.markdown("""---""")
+    commentBox.subheader("Leave Your Comment Below")
+    name = commentBox.text_input("Name")
+    comment = commentBox.text_area("Comment", placeholder="Type here")
+    submit = commentBox.button("Submit")    
 
-st_discourse(discourse_url, topic_id)
+    # Creates the comment the moment the user presses the submit button
+    if submit:
+        createComment(name, comment)
